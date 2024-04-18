410f4314fae79af80128fe3807db46087fc95a93
Luis Macías <luis@MacBook-Pro-de-Luis.local>
2024-04-18 12:17:41 -0600

    with basic configuration

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
-import { ClerkProvider } from "@clerk/nextjs";
-import "./globals.css";
+import "../globals.css";
+import Header from "@/components/shared/Header";
+import Footer from "@/components/shared/Footer";
 
 const poppins = Poppins({ 
   subsets: ["latin"],
@@ -23,10 +24,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
-      <html lang="en">
-        <body className={poppins.className}>{children}</body>
-      </html>
-    </ClerkProvider>
+    <div className="flex h-screen flex-col">
+        <Header />
+        <main className="flex-1">{children}</main>
+        <Footer />
+    </div>
   );
 }
---
+++ components/shared/Header.tsx
@@ -0,0 +1,30 @@
+import { SignedOut } from '@clerk/nextjs'
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+import { Button } from '../ui/button'
+
+const Header = () => {
+  return (
+    <header className='w-full border-b'>
+        <div className="wrapper flex items-center justify-between">
+            <Link href="/" className='w-36'>
+                <Image 
+                    src="/assets/images/logo.svg" width={128} height={38} 
+                    alt="Evently logo"
+                    className='w-[40] h-[40]'
+                />
+            </Link>
+        </div>
+        <div className="flex w-32 justify-end gap-3">
+            <SignedOut>
+                <Button asChild className="rounded-full" size="lg">
+                    <Link href="/sign-in">Login</Link>
+                </Button>
+            </SignedOut>
+        </div>
+    </header>
+  )
+}
+
+export default Header
