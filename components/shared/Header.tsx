3020a58a178d09fd52ea07ba948dc4a44bf215bf
Luis Macías <luis@MacBook-Pro-de-Luis.local>
2024-04-19 09:25:21 -0600

    some changes on main page

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,8 +1,10 @@
-import { SignedOut } from '@clerk/nextjs'
+import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import { Button } from '../ui/button'
+import NavItems from './NavItems'
+import MobileNav from './MobileNav'
 
 const Header = () => {
   return (
@@ -15,13 +17,23 @@ const Header = () => {
                     className='w-[40] h-[40]'
                 />
             </Link>
-        </div>
-        <div className="flex w-32 justify-end gap-3">
-            <SignedOut>
-                <Button asChild className="rounded-full" size="lg">
-                    <Link href="/sign-in">Login</Link>
-                </Button>
-            </SignedOut>
+                <nav className="md:flex-between hidden w-full max-w-xs">
+                    <NavItems />
+                </nav>
+            <SignedIn>
+
+            </SignedIn>
+            <div className="flex w-32 justify-end gap-3">
+                <SignedIn>
+                    <UserButton afterSignOutUrl="/" />
+                    <MobileNav />
+                </SignedIn>
+                <SignedOut>
+                    <Button asChild className="rounded-full" size="lg">
+                        <Link href="/sign-in">Login</Link>
+                    </Button>
+                </SignedOut>
+            </div>
         </div>
     </header>
   )
