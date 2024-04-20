12192fdb3c14095155e23c8cbbfd20fa57247563
Luis Macías <luis@MacBook-Pro-de-Luis.local>
2024-04-19 18:23:24 -0600

    minor changes for middleware and clerk auth webhook

--- middleware.ts
+++ middleware.ts
@@ -3,6 +3,7 @@ import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
     publicRoutes: [
         '/',
+        '/assets',
         '/api/webhook/clerk',
         '/api/webhook/stripe',
         '/api/uploadthing'
