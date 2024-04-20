671aa3cf2e59518fb0b816f37063276abb1c1eba
Luis Macías <luis@MacBook-Pro-de-Luis.local>
2024-04-20 12:43:20 -0600

    changes on user model and log to route

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -57,6 +57,7 @@ export async function POST(req: Request) {
   const { id } = evt.data;
   const eventType = evt.type;
 
+  console.log(`*** evt.data = ${evt.data}`);
   console.log(`Webhook with and ID of ${id} and type of ${eventType}`)
   console.log('Webhook body:', body)
  
